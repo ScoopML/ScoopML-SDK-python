c77be272f5d5d3b28dab718cd28784632d06b3b9
Harish S.G <harishsg99@gmail.com>
2020-11-20 17:26:03 +0530

    Update ScoopML.py

--- ScoopML/ScoopML.py
+++ ScoopML/ScoopML.py
@@ -1,1 +1,13 @@
-
+import requests
+class ScoopML():
+     
+    def __init__(self, url , text):
+        session = requests.Session()
+        self.session = session
+        self.key = url
+        self.text = text
+        
+    def get_predictions(self):
+        response = requests.post(url, data={payload: text})
+        info = response.json()
+        return info
